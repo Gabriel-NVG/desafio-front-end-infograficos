4d1f8a4834ccedc0c5740fb810b0be7405d49f69
Gabriel Gomes <gabriel_gom@hotmail.com>
2017-03-06 18:06:48 -0300

    Submenu da navbar concluído.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 $(function() {
 
-    
+    //BEGIN - Hover do submenu da navbar
+    $(".navbarSubmenu").parent().hover(function() {
+        $(this).find("ul").slideDown(100);
+    }, function() {
+        $(this).find("ul").slideUp(100);
+    });
+    //END - Hover do submenu da navbar
 
 });
